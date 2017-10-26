4eb5b8896b20a83f5bb4e7856dd6faa62449d4cb
Nathan Johnson <njohnson@ena.com>
2017-10-26 18:31:49 -0500

    updating doc.go

--- doc.go
+++ doc.go
@@ -4,39 +4,41 @@ Package radosgwadmin wraps http://docs.ceph.com/docs/master/radosgw/adminops
 Additionally, exposes some undocumented metadata operations (methods starting with 'M').
 
 Example app:
-	package main
+    package main
 
-	import (
-		"context"
-		"fmt"
-		"time"
+    import (
+        "context"
+        "fmt"
+        "time"
 
-		rgw "github.com/myENA/radosgwadmin"
-	)
+        rgw "github.com/myENA/radosgwadmin"
+        rcl "github.com/myENA/restclient"
+    )
 
-	func main() {
+    func main() {
 
-		cfg := &rgw.Config{
-			ClientTimeout:   rgw.Duration(time.Second * 10),
-			ServerURL:       "https://my.rgw.org/",
-			AdminPath:       "admin",
-			AccessKeyID:     "ABC123BLAHBLAHBLAH",
-			SecretAccessKey: "IMASUPERSECRETACCESSKEY",
-		}
+        cfg := &rgw.Config{
+            ClientConfig: rcl.ClientConfig{
+                ClientTimeout:   rcl.Duration(time.Second * 10),
+            },
+            ServerURL:       "https://my.rgw.org/",
+            AdminPath:       "admin",
+            AccessKeyID:     "ABC123BLAHBLAHBLAH",
+            SecretAccessKey: "IMASUPERSECRETACCESSKEY",
+        }
 
-		aa, err := rgw.NewAdminAPI(cfg)
-		if err != nil {
-			// do something, bail out.
-		}
-		users, err := aa.MListUsers(context.Background())
-		if err != nil {
-			// handle error
-			return
-		}
-		fmt.Println(users)
-	}
+        aa, err := rgw.NewAdminAPI(cfg)
+        if err != nil {
+            // do something, bail out.
+        }
+        users, err := aa.MListUsers(context.Background())
+        if err != nil {
+            // handle error
+            return
+        }
+        fmt.Println(users)
+    }
 
 Input validation is provided by https://godoc.org/gopkg.in/go-playground/validator.v9
-
 */
 package radosgwadmin
